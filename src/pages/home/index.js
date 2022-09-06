bffc891d3535af2e47265ad70d02e8d87c104ef2
David fox <davidfox967@yahoo.com>
2022-09-05 20:18:47 -0600

    first commit

---
+++ src/pages/home/index.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { getPets } from '../../api/petfinder';
+import Hero from '../../components/hero';
+import Pet from '../../components/pet';
+import { useParams } from 'react-router-dom';
+
+const HomePage = () => {
+  const [data, setData] = useState(null);
+  const { type } = useParams();
+  // const type = ''; // Fix me!
+
+  useEffect(() => {
+    async function getPetsData() {
+      const petsData = await getPets(type);
+      setData(petsData);
+    }
+
+    getPetsData();
+  }, [type]);
+
+  if (!data) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div className="page">
+      <Hero />
+      <h3>
+        <span className="pet-type-label">{type ? `${type}s` : 'Pets'}</span>{' '}
+        available for adoption near you
+      </h3>
+
+      {data.length ? (
+        <div className="grid">
+          {data.map((animal) => (
+            <Pet animal={animal} />
+          ))}
+        </div>
+      ) : (
+        <p className="prompt">No {type}s available for adoption now.</p>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
